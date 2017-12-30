e2eaefd487e9ee72ce60a91a2a720772aeb9f521
internetcalifornia <internetcalifornia@gmail.com>
2017-12-30 10:07:57 -0800

    iOS application

---
+++ templateViewer/templateViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  templateViewer
+//
+//  Created by Scott Eremia-Roden on 12/30/17.
+//  Copyright © 2017 nems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
